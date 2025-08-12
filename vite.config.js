05f21f273c743535ce6f996ae0b76a0def66839b
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-08-12 13:52:49 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,16 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
 export default defineConfig(({ command }) => {
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/назва-репозиторію-на-github/', // важливо для GitHub Pages
     build: {
       sourcemap: true,
       rollupOptions: {
-        // Підключаємо всі HTML-сторінки як entry, щоб працювали partials
-        input: glob.sync('./src/.html'),
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -25,18 +22,7 @@ export default defineConfig(({ command }) => {
         },
       },
       outDir: '../dist',
-      // Додаємо обробку .scss у HTML через assets
-      assetsDir: 'assets',
-      cssCodeSplit: true, // щоб CSS зберігався окремо
     },
-    plugins: [injectHTML(), FullReload(['./src/**/.html'])],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // Це дозволить писати шляхи без @import "../"
-          additionalData: '',
-        },
-      },
-    },
+    plugins: [injectHTML(), FullReload(['./src///**.html'])],
   };
 });
