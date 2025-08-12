2ba1347166ea874878c876e954e36028b22284d7
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-08-12 13:42:32 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
 export default defineConfig(({ command }) => {
   return {
     define: {
@@ -11,7 +10,6 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
