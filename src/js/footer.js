d0334f275cdf029c578fe030c656fb6606a78665
mykola-davydenko <nikolay.davydenko@eleve.efc.edu.ge>
2025-08-23 09:12:03 +0300

    mykolas js

---
+++ src/js/footer.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-footer-open]'),
+      closeModalBtn: document.querySelector('[data-modal-footer-close]'),
+      modal: document.querySelector('[data-modal-footer]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
