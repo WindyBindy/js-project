55ae9ad0ec90b575156390c9649d0aa11ee7ce00
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-08-24 09:32:24 +0200

    finished

---
+++ src/js/calculator.js
@@ -0,0 +1,47 @@
+//numbers
+const firstNumInput = document.querySelector('.calculator__input');
+const secondNumInput = document.querySelector('.calculator__num');
+//buttons functions
+const plusBtn = document.querySelector('.plusBtn');
+const mulBtn = document.querySelector('.mulBtn');
+const minusBtn = document.querySelector('.minusBtn');
+const subBtn = document.querySelector('.subBtn');
+//= button
+const isBtn = document.querySelector('.calculator__output__btn');
+//output
+const output = document.querySelector('.calculator__output');
+//----------------------------------------------------------------------
+
+output.textContent = 'результат';
+let add = '+';
+plusBtn.onclick = function () {
+  add = '+';
+};
+minusBtn.onclick = function () {
+  add = '-';
+};
+mulBtn.onclick = function () {
+  add = '*';
+};
+subBtn.onclick = function () {
+  add = '/';
+};
+
+function calculator() {
+  if (add == '+') {
+    const sum = Number(firstNumInput.value) + Number(secondNumInput.value);
+    output.textContent = sum;
+  } else if (add == '-') {
+    const sum = Number(firstNumInput.value) - Number(secondNumInput.value);
+    output.textContent = sum;
+  } else if (add == '*') {
+    const sum = Number(firstNumInput.value) * Number(secondNumInput.value);
+    output.textContent = sum;
+  } else if (add == '/') {
+    const sum = Number(firstNumInput.value) / Number(secondNumInput.value);
+    output.textContent = sum;
+  }
+}
+isBtn.onclick = function () {
+  calculator();
+};
