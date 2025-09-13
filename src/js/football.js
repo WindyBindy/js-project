e6fea20ad344e571fcd814512dec30a6f6b905cb
mykola-davydenko <nikolay.davydenko@eleve.efc.edu.ge>
2025-09-13 10:28:19 +0300

    13.09.2025

--- src/js/football.js
+++ src/js/football.js
@@ -1,7 +1,7 @@
 const field = document.getElementById("field");
 const ball = document.getElementById("ball");
 const goal = document.getElementById("goal");
-const scoreEl = document.getElementById("score");
+const scoreEl = document.getElementById("football__counter");
 
 let score = 0;
 
